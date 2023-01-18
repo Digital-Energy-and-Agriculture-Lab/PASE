7faefd456fdcf4a515b0577c53b363bb193f5bf5
RoxaneBruhwyler <roxane.bruhwyler@uliege.be>
2023-01-18 16:16:09 +0100

    Start the work of integrating pyvista functions into PV_Configuration_3D class. Add RotationAxisNumber in PV_INUTS.

--- MODULES/PHOTOVOLTAICS/configurations.py
+++ MODULES/PHOTOVOLTAICS/configurations.py
@@ -7,6 +7,7 @@ Created on Wed Jan 18 14:43:10 2023
 """
 
 import pyvista as pyV
+import numpy as np
 
 
 class PV_Configuration_3D:
@@ -15,3 +16,20 @@ class PV_Configuration_3D:
         
         self.PV_i = PV_i
         
+        self.create_first_panel()
+        
+        
+    def create_first_panel(self):
+                
+        first_panel_vertices = np.array([[-self.PV_i['PanelDimensionX']/2, self.PV_i['PanelDimensionY']/2, 0],
+                                         [self.PV_i['PanelDimensionX']/2, self.PV_i['PanelDimensionY']/2, 0],
+                                         [-self.PV_i['PanelDimensionX']/2, -self.PV_i['PanelDimensionY']/2, 0],
+                                         [self.PV_i['PanelDimensionX']/2, -self.PV_i['PanelDimensionY']/2, 0]])
+        
+        first_panel_meshes = np.hstack([[3, 0, 1, 2],    # first triangular mesh
+                                        [3, 1, 2, 3],])  # second triangular mesh
+        
+        first_panel = pyV.PolyData(first_panel_vertices, first_panel_meshes)
+        
+            
+            
