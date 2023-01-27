c9b8c02fa2175dc337159c6093aaddbd3e06a4ff
RoxaneBruhwyler <roxane.bruhwyler@uliege.be>
2023-01-27 22:59:34 +0100

    Add some comments on the numpy functions in Diffus_Direct_Proto.py

--- TEMPORARY_FILES/Diffus_Direct_Proto.py
+++ TEMPORARY_FILES/Diffus_Direct_Proto.py
@@ -22,38 +22,24 @@ def fibonacci_half_sphere(samples=18):
 
 def get_sun_position():
     
-    return [0,0,1]
+    return [0.5,0.5,0.5]
 
 
 def Get_Geometry():
     
     # PV PolyData
 
-    vertices = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 1, 0]])
+    vertices = np.array([[-2, 0, 0], [2, 0, 0], [-2, 0, 4], [2, 0, 4]])
     faces = np.hstack(
         [
             [3, 0, 1, 2],  # triangle
             [3, 1, 2, 3],  # triangle
         ]
         )
-    sPV = pv.PolyData(vertices, faces)
-
-    #Rotation of the PV
-    sPV.rotate_x(0)
-
-    # Local PV array creation
-    x,y,z = np.mgrid[-3:3, -3:3, 0:1]
-    Localmesh = pv.StructuredGrid(x, y, z)
-    LocalPVArray = Localmesh.glyph(geom=sPV, factor=0.75)
-    LocalPVArray.rotate_x(90)
-
-
-
-    # Global PV array creation
-    x,y,z = np.mgrid[-3:3, -3:3, 1:2]*5
-    GlobalMesh = pv.StructuredGrid(x, y, z)
-    GlobalPVArray  = GlobalMesh.glyph(geom=LocalPVArray, factor=0.75)
-    return GlobalPVArray
+    Wall = pv.PolyData(vertices, faces)
+    Wall.plot()
+    
+    return Wall
 
 
 
@@ -64,14 +50,14 @@ def Get_Geometry():
 Geometry = Get_Geometry() #PolyData Geometry
 
 # Sky sampling; sky location toward which a ray will be casted
-diffuseray=36
+diffuseray=72
 pTarget = fibonacci_half_sphere(diffuseray)*100
 nSkyRay= len(pTarget)
 
 
 #Mesh 
-xs = np.linspace(-5, 5, 6)
-ys = np.linspace(-5, 5, 6)
+xs = np.linspace(-5, 5, 48)
+ys = np.linspace(-5, 5, 48)
 X,Y = np.meshgrid(xs,ys)
 
 # Creation of the array of source points and target points
@@ -107,26 +93,30 @@ nSkyRay= 1
 
 
 #Mesh 
-xs = np.linspace(-5, 5, 6)
-ys = np.linspace(-5, 5, 6)
+xs = np.linspace(-5, 5, 48)
+ys = np.linspace(-5, 5, 48)
 X,Y = np.meshgrid(xs,ys)
 
 # Creation of the array of source points and target points
 # The code may be more explicit by dividing the steps
+# flatten() Return a copy of the array collapsed into one dimension
+# column_stack() Stack some 1-D arrays as columns into a 2-D array 
 SourcePoints = np.repeat(np.column_stack((X.flatten(),Y.flatten(),np.zeros(len(X.flatten())))),nSkyRay,axis=0)
+# tile() Construct an array by repeating A the number of times given by reps.
 TargetPoints = np.tile(pTarget,[len(X.flatten()),1])
 
 #Creation of the source index mapper 
 SourceI = np.repeat(np.linspace(0,len(X.flatten())-1,len(X.flatten())),nSkyRay,axis=0)
 
 #Ray Casting
-_, ind_ray, _ = Geometry.multi_ray_trace(SourcePoints,TargetPoints,first_point=True,retry=False)
+_, ind_ray, _ = Geometry.multi_ray_trace(SourcePoints,TargetPoints,first_point=False,retry=False)
 
 #Creation of the light map (1D vector but linked by index to the X.flatten(),Y.flatten())
 #1D vector allows easier implementation
 Direct = np.ones(len(X.flatten()))
 
 Touched = SourceI[ind_ray]
+# unique() Find the unique elements of an array
 unique, counts = np.unique(Touched, return_counts=True)
 
 #Computation of a 1D vector giving the direct light
