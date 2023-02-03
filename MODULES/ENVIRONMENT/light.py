0d0d0d2a3e602aab792ec59dfd9e7433535ae733
RoxaneBruhwyler <roxane.bruhwyler@uliege.be>
2023-02-03 16:18:21 +0100

    add of a method in Sun_positions class to create sun path diagram.

--- MODULES/ENVIRONMENT/light.py
+++ MODULES/ENVIRONMENT/light.py
@@ -10,13 +10,19 @@ import numpy as np
 import pandas as pd
 import pyvista as pyV
 import pvlib
+import matplotlib.pyplot as plt
+
 
 class Sun_positions:
     
-    def __init__(self, lat, long, precision_lvl):
+    def __init__(self, lat, long, precision_lvl, loc_name):
         
+        self.lat = lat
+        self.long = long
+        self.loc_name = loc_name
         self.get_solar_positions(lat, long, precision_lvl)
         self.get_sun_vector(self.SP['elevation'], self.SP['azimuth'])
+        self.get_sun_path_diagram()
         
         
     def get_solar_positions(self, lat, long, precision_lvl):
@@ -29,20 +35,29 @@ class Sun_positions:
         hour = index.hour
         month = index.month
         week_id = index.weekofyear
-        solar_position.insert(0, "hour", hour)
-        solar_position.insert(1, "month", month)
-        solar_position.insert(2, "week", week_id)
+        julian_day = index.dayofyear
         
-        if precision_lvl == 1:
+        solar_position.insert(0, "hour", hour)
+        solar_position.insert(1, "J_day", julian_day)
+        solar_position.insert(2, "month", month)
+        solar_position.insert(3, "week", week_id)
+        
+        if precision_lvl == 1:           
             SP_month = solar_position.groupby(by=['month','hour']).mean()
             self.SP = SP_month[SP_month['elevation']>=0]
+            self.SP = self.SP.set_index('week', append=True)
         
-        elif precision_lvl == 2:
+        elif precision_lvl == 2:  
             SP_week = solar_position.groupby(by=['week','hour']).mean()
             self.SP = SP_week[SP_week['elevation']>=0]
-            
+            self.SP = self.SP.set_index('month', append=True)
+                        
         else:
             self.SP = solar_position[solar_position['elevation']>=0]
+            self.SP = self.SP.set_index('month', append=True)
+            self.SP = self.SP.set_index('hour', append=True)
+            self.SP = self.SP.set_index('week', append=True)
+            
                
          
     def get_sun_vector(self, beta, gamma):
@@ -55,8 +70,49 @@ class Sun_positions:
         self.solar_vector[:,1]=np.cos(gamma)*np.cos(beta)
         self.solar_vector[:,2]=np.sin(beta)
         #SOURCE : Kevin Anderson and Mark Mikofski, Slope-Aware Backtracking for Single-Axis Trackers, NREL
-
         
+    def get_sun_path_diagram(self):
+        
+        #SOURCE : https://pvlib-python.readthedocs.io/en/stable/gallery/solar-position/plot_sunpath_diagrams.html#sphx-glr-gallery-solar-position-plot-sunpath-diagrams-py
+        
+        fig = plt.figure()        
+        ax = plt.subplot(1, 1, 1, projection='polar')
+        points = ax.scatter(np.radians(self.SP.azimuth), self.SP.apparent_zenith,
+                            s=2, label=None, c=self.SP.J_day.round(0))
+        ax.figure.colorbar(points)
+
+        self.SP = self.SP.reset_index(level='hour')
+
+        # draw hour labels
+        SP_june = self.SP.query("month == 6")
+        for h in np.unique(SP_june.hour):
+            # choose label position by the smallest radius for each hour
+            subset = SP_june.loc[SP_june['hour'] == h]
+            r = subset.apparent_zenith
+            pos = subset.loc[r.idxmin(),:]
+            ax.text(np.radians(pos['azimuth']), pos['apparent_zenith'], str(h))
+         
+        # draw individual days
+        for date in pd.to_datetime(['2019-03-21', '2019-06-21', '2019-12-21']):
+            times = pd.date_range(date, date+pd.Timedelta('24h'), freq='5min')
+            solpos = pvlib.solarposition.get_solarposition(times, 
+                                                           self.lat,
+                                                           self.long)
+            solpos = solpos.loc[solpos['apparent_elevation'] > 0, :]
+            label = date.strftime('%Y-%m-%d')
+            ax.plot(np.radians(solpos.azimuth), solpos.apparent_zenith, label=label)
+
+        ax.figure.legend(loc='upper left')
+
+        # change coordinates to be like a compass
+        ax.set_theta_zero_location('N')
+        ax.set_theta_direction(-1)
+        ax.set_rmax(90)
+
+        fig.savefig('OUTPUTS/GRAPHS/SunPathDiagram_'+self.loc_name+'.svg')
+        
+    
+       
         
 class Shade_direct_light:
 
@@ -106,9 +162,9 @@ class Shade_direct_light:
             
 class Sky_view_factor:
     
-    def __init__(self, meshgrid, PV_central):
+    def __init__(self, meshgrid, PV_central, n_small_suns):
         
-        n_small_suns = 108
+        
         pTarget = self.fibonacci_half_sphere(n_small_suns)
             
         
@@ -158,4 +214,34 @@ class Sky_view_factor:
         self.diffuse_map_t = Diffu.reshape(len(meshgrid.X[:,0]),len(meshgrid.Y[0,:])).transpose()
             
 
-        
+def show_light_map(light_matrix, msh_grid, PV_central):
+    
+    grid = pyV.StructuredGrid(msh_grid.X, msh_grid.Y, np.ones((len(msh_grid.X[:,0]),len(msh_grid.X[0,:])))*0.05)
+
+    test1 = light_matrix.ravel()
+
+    plotter = pyV.Plotter()
+
+    plotter.add_mesh(PV_central, color='black')
+    ground = np.array([[-100, 100, 0],
+                       [100, 100, 0],
+                       [-100, -100, 0],
+                       [100, -100, 0]])
+
+    ground_m = np.hstack([[3, 0, 1, 2],    
+                          [3, 1, 2, 3],])
+
+    grnd = pyV.PolyData(ground, ground_m)
+
+    plotter.add_mesh(grnd, color='green')
+    plotter.show_axes()
+
+    plotter.add_mesh(
+        grid,
+        scalars=test1,
+        lighting=False,
+        show_edges=True,
+        scalar_bar_args={"title": "Height"},
+        clim=[0, 1])
+
+    plotter.show()
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from MODULES.DATA_MANAGEMENT.yaml_inputs_provider import YAML_Inputs_provider
 from MODULES.PHOTOVOLTAICS.configurations import PV_Configuration_3D
 from MODULES.ENVIRONMENT.light import Sun_positions
 from MODULES.ENVIRONMENT.environment_config import Plane_Ground_regular_meshes
-from MODULES.ENVIRONMENT.light import Shade_direct_light, Sky_view_factor
+from MODULES.ENVIRONMENT.light import Shade_direct_light, Sky_view_factor, show_light_map
 
 PASE_Logger()
 
@@ -22,53 +22,72 @@ PV_1 = YAML_Inputs_provider(file='PV_central.yaml').i
 
 PV_1_3Dconfig = PV_Configuration_3D(PV_1)
 
-Sun_positions = Sun_positions(Loc_1['Latitude'], Loc_1['Longitude'], Loc_1['PrecisionLevelOnSunPosition'])
+Sun_positions = Sun_positions(Loc_1['Latitude'],
+                              Loc_1['Longitude'],
+                              Loc_1['PrecisionLevelOnSunPosition'],
+                              Loc_1['LocationName'])
 
 msh_grid = Plane_Ground_regular_meshes(Loc_1['Xmin_InterestZone'], Loc_1['Xmax_InterestZone'],
                                        Loc_1['Ymin_InterestZone'], Loc_1['Ymax_InterestZone'],
                                        Loc_1['dX_InterestZone'], Loc_1['dY_InterestZone'])
 
-Direct_light_map = Shade_direct_light(msh_grid, PV_1_3Dconfig.PV_central, Sun_positions.solar_vector)
+#Direct_light_map = Shade_direct_light(msh_grid, PV_1_3Dconfig.PV_central, Sun_positions.solar_vector)
 
-Diffuse_light_map = Sky_view_factor(msh_grid, PV_1_3Dconfig.PV_central)
+#Diffuse_light_map = Sky_view_factor(msh_grid, PV_1_3Dconfig.PV_central, 360)
 
+#show_light_map(Direct_light_map.direct_map_t[17,:,:], msh_grid, PV_1_3Dconfig.PV_central)
 
+#show_light_map(Diffuse_light_map.diffuse_map_t, msh_grid, PV_1_3Dconfig.PV_central)
 
 """
-import pyvista as pv
+
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import pvlib
 
-grid = pv.StructuredGrid(msh_grid.X, msh_grid.Y, np.ones((len(msh_grid.X[:,0]),len(msh_grid.X[0,:])))*0.05)
+fig = plt.figure()        
+ax = plt.subplot(1, 1, 1, projection='polar')
+points = ax.scatter(np.radians(Sun_positions.SP.azimuth), Sun_positions.SP.apparent_zenith,
+                    s=2, label=None, c=Sun_positions.SP.J_day.round(0))
+ax.figure.colorbar(points)
 
-test1 = Diffuse_light_map.diffuse_map_t[:,:].ravel()
+Sun_positions.SP = Sun_positions.SP.reset_index(level='hour')
 
-plotter = pv.Plotter()
+# draw hour labels
+SP_june = Sun_positions.SP.query("month == 6")
+for h in np.unique(SP_june.hour):
+    # choose label position by the smallest radius for each hour
+    subset = SP_june.loc[SP_june['hour'] == h]
+    r = subset.apparent_zenith
+    print(r)
+    pos = subset.loc[r.idxmin(),:]
+    ax.text(np.radians(pos['azimuth']), pos['apparent_zenith'], str(h))
+ 
+# draw individual days
+for date in pd.to_datetime(['2019-03-21', '2019-06-21', '2019-12-21']):
+    times = pd.date_range(date, date+pd.Timedelta('24h'), freq='5min')
+    solpos = pvlib.solarposition.get_solarposition(times, 
+                                                   Sun_positions.lat,
+                                                   Sun_positions.long)
+    solpos = solpos.loc[solpos['apparent_elevation'] > 0, :]
+    label = date.strftime('%Y-%m-%d')
+    ax.plot(np.radians(solpos.azimuth), solpos.apparent_zenith, label=label)
 
-plotter.add_mesh(PV_1_3Dconfig.PV_central, color='black')
-ground = np.array([[-100, 100, 0],
-                   [100, 100, 0],
-                   [-100, -100, 0],
-                   [100, -100, 0]])
+ax.figure.legend(loc='upper left')
 
-ground_m = np.hstack([[3, 0, 1, 2],    
-                      [3, 1, 2, 3],])
+# change coordinates to be like a compass
+ax.set_theta_zero_location('N')
+ax.set_theta_direction(-1)
+ax.set_rmax(90)
 
-grnd = pv.PolyData(ground, ground_m)
-
-plotter.add_mesh(grnd, color='green')
-plotter.show_axes()
-
-plotter.add_mesh(
-    grid,
-    scalars=test1,
-    lighting=False,
-    show_edges=True,
-    scalar_bar_args={"title": "Height"},
-    clim=[0, 1])
-
-plotter.show()
+fig.savefig('OUTPUTS/GRAPHS/SunPathDiagram_'+Sun_positions.loc_name+'.svg')
 
 """
+
+
+
+
 
 
 
