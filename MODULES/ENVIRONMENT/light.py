[fix and feat] Modify the rotation around z axis with the azimut parameter considering the NREL formalism as for the calculation of GTI and add of North, East and zenith in the reference axis of the Plotter for the user to be modre confident with the congifuration of the central. Import the base case of the Siguesol demonstrator.

MODULES/ENVIRONMENT/light.py
@@ -536,7 +536,9 @@ def show_light_map(light_matrix, msh_grid, PV_central, lim_min, lim_max, lgd_tit
     grid = pyV.StructuredGrid(msh_grid.X, msh_grid.Y, np.ones((len(msh_grid.X[:,0]),len(msh_grid.X[0,:])))*0.05)
 
     test1 = light_matrix.ravel()
-
+    
+    labels = dict(zlabel='Z (ZENITH)', xlabel='X (EAST)', ylabel='Y (NORTH)')
+    
     plotter = pyV.Plotter()
 
     plotter.add_mesh(PV_central, color='black')
@@ -551,7 +553,8 @@ def show_light_map(light_matrix, msh_grid, PV_central, lim_min, lim_max, lgd_tit
     grnd = pyV.PolyData(ground, ground_m)
 
     plotter.add_mesh(grnd, color='green')
-    plotter.show_axes()
+    #plotter.show_axes()
+    plotter.add_axes(**labels)
 
     plotter.add_mesh(
         grid,

main.py
@@ -27,8 +27,12 @@ PASE_Logger()
 
 Loc_1 = YAML_Inputs_provider(file='Siguesol_loc.yaml').i
 
+PV_1 = YAML_Inputs_provider(file='PV_central_siguesol.yaml').i
+
+"""
 PV_1 = YAML_Inputs_provider(file='PV_central_east.yaml').i
 PV_2 = YAML_Inputs_provider(file='PV_central_west.yaml').i
+"""
 
 WD = Weather_data(Loc_1['Latitude'],
                   Loc_1['Longitude'],
@@ -50,6 +54,9 @@ Sun_positions_samp = Sun_positions_sampled(Loc_1['Latitude'],
 PV_1_3Dconfig = PV_Configuration_3D(PV_1, Sun_positions_samp.solar_vector,
                                     visualization=True)                        # !!!! Problem with rotation angle that are negative
 
+#Example of a way to combine multiple configurations of PV rows and integration of a barrier 
+#(Nicolas started to integrate the combination of mutpliple PV_central files in functions in the MaiBis.py)
+"""
 PV_2_3Dconfig = PV_Configuration_3D(PV_2, Sun_positions_samp.solar_vector,
                                     visualization=True) 
 
@@ -72,6 +79,7 @@ merged = PV_1_3Dconfig.PV_central.merge(PV_2_3Dconfig.PV_central)
 merged2 = merged.merge(barrieres)
 merged2.plot(style='wireframe', color='tan')
 ####
+"""
 
 msh_grid = Plane_Ground_regular_meshes(Loc_1['Xmin_InterestZone'], Loc_1['Xmax_InterestZone'],
                                        Loc_1['Ymin_InterestZone'], Loc_1['Ymax_InterestZone'],
@@ -89,7 +97,7 @@ Light = Light(WD.nyears, Sun_positions)
 PV_central = PV_system(PV_1)
 
 
-shade_scene = Light_shade_scene(msh_grid, merged2)
+shade_scene = Light_shade_scene(msh_grid, PV_1_3Dconfig.PV_central)
 shade_scene.get_light_map(360, Sun_positions_samp.solar_vector)
 
 shade_scene.get_daily_irradiation_map(Sun_positions_samp.SP,
@@ -103,18 +111,18 @@ PV_central.get_electricity_production(Sun_positions, Light.data, WD.nyears)
 
 ### VISUALISATION (temporary)
 
-show_light_map(shade_scene.dir_map[:,:,7], msh_grid, merged2, 0, 1, "Relative direct light reaching the ground [-]")
+show_light_map(shade_scene.dir_map[:,:,1], msh_grid, PV_1_3Dconfig.PV_central, 0, 1, "Relative direct light reaching the ground [-]")
 # IF there is one rotation axis
 #show_light_map(shade_scene.dir_map[:,:,5], msh_grid, PV_1_3Dconfig.PV_central[5])
 
-show_light_map(shade_scene.diff_map.astype(np.float32), msh_grid, merged2, 0, 1, "Sky visibility factor [-]")
+show_light_map(shade_scene.diff_map.astype(np.float32), msh_grid, PV_1_3Dconfig.PV_central, 0, 1, "Sky visibility factor [-]")
 
 
 #temporary lines
 j=2
 show_light_map(shade_scene.daily_irr_spat['2005'][:,:,j],
                msh_grid,
-               merged2,
+               PV_1_3Dconfig.PV_central,
                shade_scene.daily_irr_spat['2005'][:,:,j].min(),
                shade_scene.daily_irr_spat['2005'][:,:,j].max(),
                "Total irradiation reaching the ground on the julian day "+str(j)+" [MJ/m²]")