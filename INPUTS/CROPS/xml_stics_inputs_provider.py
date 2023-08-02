99b3c68f7477549f1848bddfb25671f6eaf07cea
RoxaneBruhwyler <roxane.bruhwyler@uliege.be>
2023-08-02 18:36:04 +0200

    [feature] Add of the possibility to import plt, ini, tec, sta and sols .xml parametrisation files of STICS. Would be great to implemant a method to verify value of parameters are between the min and max values as it is done for the YAML files.

--- INPUTS/CROPS/xml_stics_inputs_provider.py
+++ INPUTS/CROPS/xml_stics_inputs_provider.py
@@ -10,29 +10,111 @@ import xmltodict
             
 class XML_STICS_Inputs_Provider(dict):
     
-    def __init__(self, file=None):
+    def __init__(self, file=None, soil_name=None):
         
         dict.__init__(self)
-        dict.__setitem__(self, 'coucou', 25)
         
         rfile = open(file,"r")
         xml_dict = xmltodict.parse(rfile.read())
         
-        for formalism in xml_dict['fichierplt']['formalisme']:
-            
-            for key, data in formalism.items():
+        if ('plt.xml' in file or 'tec.xml' in file or 'sta.xml' in file) and soil_name is None:
+            file_type = next(iter(xml_dict))
+            for formalism in xml_dict[file_type]['formalisme']:
+                self.retrieve_parameters(formalism)
                 
-                if key == 'param' and type(data) is list:
-                    for parameter in data:
-                        dict.__setitem__(self, parameter['@nom'], num(parameter['#text']))
-                elif key == 'param' and type(data) is dict:
-                    dict.__setitem__(self, data['@nom'], num(data['#text']))
-                elif key == 'option' and type(data) is list:
-                    for parameter in data:
-                        dict.__setitem__(self, parameter['@nomParam'], num(parameter['@choix']))
-                elif key == 'option' and type(data) is dict:
-                    dict.__setitem__(self, data['@nomParam'], num(data['@choix']))
-                                    
+        elif 'sols.xml' in file and soil_name is not None:          
+            for soil in xml_dict['sols']['sol']:
+                if soil['@nom'] == soil_name:
+                    self.retrieve_parameters(soil)
+                    
+        elif 'ini.xml' in file and soil_name is None:
+            self.retrieve_init_parameters(xml_dict)
+            
+                    
+    def retrieve_parameters(self, dictionnary=None):
+        
+        for key, data in dictionnary.items():
+                       
+            if key == 'param' and type(data) is list:
+                for parameter in data:
+                    dict.__setitem__(self, parameter['@nom'], num(parameter['#text']))
+                    
+                    
+            elif key == 'param' and type(data) is dict:
+                dict.__setitem__(self, data['@nom'], num(data['#text']))
+                
+                
+            elif key == 'option' and type(data) is list:
+                for model_choice in data:
+                    dict.__setitem__(self, model_choice['@nomParam'], num(model_choice['@choix']))
+                    for choice in model_choice['choix']:
+                        for key2, data2 in choice.items():
+                            if key2 == 'param' and type(data2) is list:
+                                for parameter2 in data2:
+                                    dict.__setitem__(self, parameter2['@nom'], num(parameter2['#text']))
+                            elif key2 == 'param' and type(data2) is dict:
+                                dict.__setitem__(self, data2['@nom'], num(data2['#text']))
+                    
+            elif key == 'option' and type(data) is dict:
+                dict.__setitem__(self, data['@nomParam'], num(data['@choix']))
+                for choice in data['choix']:
+                    for key2, data2 in choice.items():
+                        if key2 == 'param' and type(data2) is list:
+                            for parameter2 in data2:
+                                dict.__setitem__(self, parameter2['@nom'], num(parameter2['#text']))
+                        elif key2 == 'param' and type(data2) is dict:
+                            dict.__setitem__(self, data2['@nom'], num(data2['#text']))
+             
+                
+            elif key == 'ta':
+                interventions_dict = {}
+                if type(data['intervention']) is list:
+                    for inter in data['intervention']:
+                        params = {}
+                        for par in inter['colonne']:
+                            params[par['@nom']] = num(par['#text'])
+                        interventions_dict[inter['colonne'][0]['#text']] = params
+                elif type(data['intervention']) is dict:
+                    params = {}
+                    for par in data['intervention']['colonne']:
+                        params[par['@nom']] = num(par['#text'])
+                    interventions_dict[data['intervention']['colonne'][0]['#text']] = params
+                dict.__setitem__(self, dictionnary['@nom'], interventions_dict)
+                
+    
+    def retrieve_init_parameters(self, dictionnary):
+        
+        dict.__setitem__(self, 'nbplantes', num(dictionnary['initialisations']['nbplantes']))
+        for key, data in dictionnary['initialisations']['plante'][0].items():
+            if type(data) is str:
+                dict.__setitem__(self, key, num(data))
+            else:
+                layer_dic = {}
+                for layer in data['horizon']:
+                    layer_dic[layer['@nh']] = num(layer['#text'])
+                dict.__setitem__(self, key, layer_dic)
+                
+        for key, data in dictionnary['initialisations']['sol'].items():
+            layer_dic = {}
+            for layer in data['horizon']:
+                layer_dic[layer['@nh']] = num(layer['#text'])
+            dict.__setitem__(self, key, layer_dic)
+
+        if num(dictionnary['initialisations']['nbplantes']) == 2:
+            dic_2nd_plant = {}
+            for key, data in dictionnary['initialisations']['plante'][1].items():
+                if type(data) is str:
+                    dic_2nd_plant[key] = num(data)
+                elif type(data) is type(None):
+                    dic_2nd_plant[key] = data
+                else:
+                    layer_dic = {}
+                    for layer in data['horizon']:
+                        layer_dic[layer['@nh']] = num(layer['#text'])
+                    dic_2nd_plant[key] = layer_dic
+            dict.__setitem__(self, '2nd_plant', dic_2nd_plant)
+        
+        
 def num(x):
     try:
         return int(x)
@@ -45,3 +127,9 @@ def num(x):
     
     
 cropdata = XML_STICS_Inputs_Provider("wheat_plt.xml")
+agromanagement = XML_STICS_Inputs_Provider("Ble_tec.xml")
+soildata = XML_STICS_Inputs_Provider("sols.xml", 'solcanne')
+init_data = XML_STICS_Inputs_Provider("ble_ini.xml")
+station_data = XML_STICS_Inputs_Provider("climblej_sta.xml")
+
+            
