b566e4c642bf726f25002ae67ed273f0f4801b27
Louis Lemaire <louis.lemaire@uliege.be>
2023-11-20 10:35:37 +0100

    [feat] GrasSim - if ETP is not available in weather data, it is computed

--- MODULES/CROPS/GRASSIM/call_grassim.py
+++ MODULES/CROPS/GRASSIM/call_grassim.py
@@ -9,6 +9,7 @@ import pandas as pd
 from MODULES.CROPS.GRASSIM import grassim_model
 from MODULES.DATA_MANAGEMENT.yaml_inputs_provider import YAML_Inputs_provider
 from MODULES.CROPS.SIMPLE.evapotranspiration_FAO56_PM import get_ET0
+from MODULES.CROPS.GRASSIM.weather_data_topandas import weather_data_topandas
 
 
 def call_grassim(WD, daily_irr, lat, alt):
@@ -19,7 +20,10 @@ def call_grassim(WD, daily_irr, lat, alt):
     PFT_values = pd.read_csv('INPUTS\\CROPS\\GRASSIM\\Parameters_values_PFT.csv',header=0, sep=";", decimal='.')
     Management = YAML_Inputs_provider(file = 'CROPS\\GRASSIM\\Management.yml').i #need to add duration parameter
     albedo = 0.2 #needs to be in crop init parameters. Not available for GEMBLOUX crop.
-
+    
+    weather_option = 1
+    if weather_option:
+        WD = weather_data_topandas('INPUTS\\CROPS\\GRASSIM\\Weather_Gembloux_2016.csv')
     
     Soil_plot = 0 #for now, all of the output is stored in Crop_plot
     Crop_plot = grassim_model.Crop(crop_init=Crop_init, Kc_values=Kc_values, PFT_composition=PFT_composition, PFT_values=PFT_values, Management=Management)
@@ -30,21 +34,24 @@ def call_grassim(WD, daily_irr, lat, alt):
         Crop_plot.init_crop(daily_irr[year])
     
         for day in WD[year].index:
+
+            irradiation = daily_irr[year][:,day.day_of_year-1]
             
-            irradiation = daily_irr[year][:,day.day_of_year-1].reshape((20, 20))
-            
-            ET0 = get_ET0(WD[year]['Avg_temp'][day],
-                          WD[year]['Min_temp'][day],
-                          WD[year]['Max_temp'][day],
-                          WD[year]['Avg_WS_2m'][day],
-                          WD[year]['Vap_press'][day],
-                          irradiation,
-                          Crop_plot.LAI,
-                          day.day_of_year,
-                          len(WD[year]['Avg_temp']),
-                          lat,
-                          alt,
-                          albedo)
+            if 'ETP' in WD[year].columns:
+                ET0 = WD[year]['ETP'][day]
+            else :
+                ET0 = get_ET0(WD[year]['Avg_temp'][day],
+                              WD[year]['Min_temp'][day],
+                              WD[year]['Max_temp'][day],
+                              WD[year]['Avg_WS_2m'][day],
+                              WD[year]['Vap_press'][day],
+                              irradiation,
+                              Crop_plot.LAI,
+                              day.day_of_year,
+                              len(WD[year]['Avg_temp']),
+                              lat,
+                              alt,
+                              albedo)
             
             Crop_plot.run_grassim_model(WD[year].loc[day], ET0, irradiation, day)
 
