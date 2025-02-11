[fix] days_since_rotation is updated if day is outside of a rotation period
* Place return False after the for loop.
* Add self.days_since_rotation += 1 before return False

MODULES/CROPS/GRASSIM/management/management.py
@@ -196,7 +196,8 @@ class Management():
                     else:
                         self.days_since_rotation = 1
                         return True
-                return False
+            self.days_since_rotation += 1
+            return False
                     
         elif self.config['rotationType'] == "sward_height":
             mean_paddock_sward_height = np.mean(crop.sward_height[self.current_mask])