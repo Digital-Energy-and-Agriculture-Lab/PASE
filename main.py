268ac8341e15427a464d48de308866bbaf898e9a
Arnaud Bouvry <abouvry@uliege.be>
2025-03-12 10:17:45 +0100

    [feature] Temporary - crop yield map display in a conditional and automate crop yield display as last simulation year
    The input of the function used to display results in the main and example scripts is not called identically for the different crop models, so I put a conditional to avoid having to manually adapt the scripts every time we switch crop models.
    
    Also automatically select the last simulation year to display the crop yield, based on the parameters dictionary. Until now, it was hard coded. It made it so the user could run into an error when the hard-coded year did not match the time range of the simulation. Advanced users can still edit the date of the yield map display as needed.

--- main.py
+++ main.py
@@ -204,7 +204,15 @@ Soil_plot, Crop_plot = run_crop_simu(crop_config, option_2D, WD.nyears_daily_dat
 
 save_csv('mean_data.csv', Crop_plot.nyears_data, ['Biomass', 'Dry_yield'])
 
-show_light_map2(L.sourcepoints[:,:-1], 
-                Crop_plot[0].nyears_data['2008']['BM'][datetime.strptime('2008-10-10 00:00:00', '%Y-%m-%d %H:%M:%S')]/100,
-                PV_1_3Dconfig.PV_central_PD,
-                "Biomass Gras-Sim 2008 [t/ha]")
+crop_display_year = str(Loc_1['SimulationEndingYear'])
+if crop_config['CropModel'] == 'grassim':
+    show_light_map2(L.sourcepoints[:,:-1],
+                    Crop_plot[0].nyears_data[crop_display_year]['BM'][datetime.strptime(crop_display_year+'-10-10 00:00:00', '%Y-%m-%d %H:%M:%S')]/100,
+                    PV_1_3Dconfig.PV_central_PD,
+                    "Biomass Gras-Sim "+crop_display_year+" [t/ha]")
+elif crop_config['CropModel'] == 'simple':
+    show_light_map2(L.sourcepoints[:, :-1],
+                    Crop_plot.nyears_data[crop_display_year]['Dry_yield'][
+                        crop_display_year+'-10-10 00:00:00'] / 100,
+                    PV_1_3Dconfig.PV_central_PD,
+                    "Dry yield SIMPLE "+crop_display_year+" [t/ha]")
