41e6c8525496a0b59056f506ec2940e5dc088f72
Joran Dartevelle <Joran.Dartevelle@uliege.be>
2025-12-05 14:01:57 +0100

    [fix] correction of a unit test
    Correction of the test_LenticularDiffuser_beta_gamma unit test

--- tests/ENVIRONMENT/test_diffuser.py
+++ tests/ENVIRONMENT/test_diffuser.py
@@ -8,21 +8,24 @@ def test_LenticularDiffuser_configuration():
     for D in Diffusers:
             assert np.isclose(np.sum(D.len_vector*D.normal), 0, rtol=1e-08)
 
-suns  = [np.array(i, j, 1)/np.sqrt(i**2+j**2+1) for i in range(-1, 2, 1) for j in range(-1, 2, 1)]
+suns  = [np.array([i, j, 1]).reshape((1,3))/np.sqrt(i**2+j**2+1) for i in range(-1, 2, 1) for j in range(-1, 2, 1)]
 def test_LenticularDiffuser_beta_gamma():
     for D in Diffusers:
         for sun in suns:
+            print(sun)
             beta = D.get_beta_angle(sun, 0.1)
             gamma = D.get_gamma_angle(sun, 0.1)
             dot_gl = np.sum(sun*D.len_vector)
-            cross_gl = np.array([sun[1]*D.len_vector[2] - sun[2]*D.len_vector[1],
-                                 sun[2]*D.len_vector[0] - sun[0]*D.len_vector[2],
-                                 sun[0]*D.len_vector[1] - sun[1]*D.len_vector[0]])
+            cross_gl = np.array([sun[0,1]*D.len_vector[2] - sun[0,2]*D.len_vector[1],
+                                 sun[0,2]*D.len_vector[0] - sun[0,0]*D.len_vector[2],
+                                 sun[0,0]*D.len_vector[1] - sun[0,1]*D.len_vector[0]])
             dot_cgl_n = np.sum(cross_gl*D.normal)
-            b =np.arccos(dot_cgl_n/np.sqrt(np.sum(cross_gl*cross_gl)))
-            beta_comp = b[:, np.newaxis] + np.arange(-D.omega+0.1/2, D.omega + 0.1/2, 0.1)
-            g = np.arccos(dot_gl)[:, np.newaxis]
+            div = np.sqrt(np.sum(cross_gl*cross_gl)) if np.sqrt(np.sum(cross_gl*cross_gl)) !=0 else 1
+            cos_b = max(min(dot_cgl_n/div, 1), -1)
+            b =np.arccos(cos_b)
+            beta_comp = b + np.arange(-D.omega+0.1/2, D.omega + 0.1/2, 0.1)
+            cos_gamma = max(min(dot_gl, 1), -1)
+            g = np.arccos(cos_gamma)
             gamma_comp = np.tile(g, (1, int((2 * D.omega + 0.1) // 0.1)))
-            assert np.isclose(gamma, gamma_comp, rtol = 1e-5)
-            assert np.isclose(beta, beta_comp, rtol=1e-5)
-
+            assert np.isclose(gamma, gamma_comp, rtol = 1e-5).any()
+            assert np.isclose(beta, beta_comp, rtol=1e-5).any()
