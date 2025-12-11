dad04436912b0d31be8c40d052f1314621f6fe6c
Arnaud Bouvry <abouvry@uliege.be>
2025-12-11 14:57:34 +0100

    [chore] Update import of save_mean_to_csv() in main.py
    Update import of save_csv() to save_mean_to_csv(), as well as path (output is now OUTPUT)

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from pase.user_support_tools import PASE_Logger
 from pase.DATA_MANAGEMENT.yaml_inputs_provider import YAML_Inputs_provider, Inputs_aggregator
 from pase.DATA_MANAGEMENT.input_checker import InputsEvaluator
 from pase.DATA_MANAGEMENT.weather_data_provider import Weather_data
-from pase.DATA_MANAGEMENT.output.save_csv import save_csv
+from pase.DATA_MANAGEMENT.OUTPUT.save_csv import save_mean_to_csv
 from pase.PHOTOVOLTAICS.configuration import PV_Configuration_3D
 from pase.ENVIRONMENT.light import Sun_positions_sampled, Sun_positions, Light
 from pase.ENVIRONMENT.light import Ray_casting_scene
@@ -243,9 +243,9 @@ if crop_config['CropModel'] == ('simple' or 'stics'):
     visualize_map_of_a_variable(crop_config, agro_results, 'Fresh_yield',
                                 PV_1_3Dconfig.PV_central_PD, M, 2008, 
                                 MM_DD='10-10', unit='g/m²')
-    save_csv('mean_data.csv', agro_results, ['Dry_yield', 'Biomass'])
+    save_mean_to_csv('mean_data.csv', agro_results, ['Dry_yield', 'Biomass'])
 else:
     visualize_map_of_a_variable(crop_config, agro_results,'BM',
                                 PV_1_3Dconfig.PV_central_PD, M, 2008,
                                 MM_DD='10-10', unit='t/ha')
-    save_csv('mean_data.csv', agro_results, ['BM'])
+    save_mean_to_csv('mean_data.csv', agro_results, ['BM'])
