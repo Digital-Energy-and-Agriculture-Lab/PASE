30fb55f1f699e34eed3c09fefe215cf6820f1ba4
Joran Dartevelle <Joran.Dartevelle@uliege.be>
2026-02-27 15:47:53 +0100

    [docs] add explainations, units and size
    add explainations, units and size in docstring
    Remove old functions

--- pase/ENVIRONMENT/diffuser.py
+++ pase/ENVIRONMENT/diffuser.py
@@ -12,32 +12,48 @@ class Diffuser:
     General class for diffusers, allowing transfer functions not to be specified.
     Must be completed (by inheritance) by a specific diffuser class such as Lenticular_diffuser.
 
-    azimut_diff : Diffuser azimuth
-    elevation_diff : Diffuser elevation
-    geometry : Diffuser geometry (pyVista)
+    Input
+        azimuth: Diffuser azimuth in the global reference frame in degrees.
+        elevation: Diffuser elevation in the global reference frame in degrees.
+
+    Attributes:
+        x_dr, y_dr, z_dr: Transmitted ray directions in the diffuser reference frame.
+        azimuth_diff, elevation_diff: Diffuser azimuth and elevation in the global reference frame in radians
+                                      using the trigonometric convention for defining angles.
+
+    Diffuser reference frame (e_x, e_y, e_z):
+        e_z: Normal of the diffuser plane
+        e_y: unit vector in the direction of the diffuser lenses in the case of a lenticular diffuser
+        e_x: Cross product between e_y and e_z
     """
 
-    def __init__(self, azimuth, elevation, kernel='gaussian'):
+    def __init__(self, azimuth, elevation):
         self.x_dr = None
         self.z_dr = None
         self.y_dr = None
         self.azimuth_diff = -np.radians(azimuth-90)
         self.elevation_diff = np.radians(elevation)
-        self.kernel = kernel
 
     def generate_direction_diffuser_referential(self, vect_sun, angle_discr):
         """
         Call the transfer function to compute the direction of the transferred light
         with a given resolution.
-        Input :
-            vect_sun : the sun vectors
-            res : the angle resolution for the transmitted light direction in degrees
+        Input:
+            vect_sun: the sun vectors (nSunPos x 3)
+            angle_discr: the angle resolution for the transmitted light direction in degrees
+
+        Output:
+            x_dr, y_dr, z_dr: Transmitted ray directions in the diffuser reference frame (nSunPos x Nvec x 3).
+            rho: energy contained in each vector (nSunPos x Nvec)
+            ds: Size of the segment belonging to the vector (nSunPos x Nvec)
         """
         self.x_dr, self.y_dr, self.z_dr, self.rho, self.ds = self.transfer_function(vect_sun, angle_discr)
 
     def get_direction_sky_referential(self):
         """
         Call the function to switch to the sky reference frame
+        Output:
+            x_sr, y_sr, z_sr: Transmitted ray directions in the global reference frame (nSunPos x Nvec x 3).
         """
         y_vector = np.array([0, 1, 0])
         z_vector = np.array([0, 0, 1])
@@ -49,65 +65,69 @@ class Diffuser:
     def get_light_direction(self, vect_sun, discr, sigma, angle_discr):
         """
         General function to compute the directions and transform them in the right frame.
-        Input :
-            vect_sun : sun vectors
+        Input:
+            vect_sun: sun vectors (nSolPos x 3)
+            discr: Direction of the sky patches (nPatch x 3)
+            sigma: Size of the sky patch (nPatch)
+            angle_discr: angular resolution of transmitted rays created.
+        Output:
+            W: Weight of the transmitted rays associate with each patch (nSolPos x nPatch)
         """
         self.generate_direction_diffuser_referential(vect_sun, angle_discr)
         self.get_direction_sky_referential()
-        W = self.get_discretized_BSDF(discr, sigma, self.kernel)
+        W = self.get_discretized_BSDF(discr, sigma)
         return W
 
-    def get_BTDF_plot(self, azimuth, zenith):
-        """
-        Create the bidirectional transmittance distribution function (BTDF) plot for the
-        given azimuth and zenith data.
-        """
-        azimuth = np.radians(270-azimuth)
-        fig, ax = plt.subplots(subplot_kw=dict(projection='polar'))
-        ax.plot(azimuth, zenith)
-        ax.set_ylim(0, 90)
-        ax.set_xticks(np.radians[0, 45, 90, 135, 180, 225, 270, 315])
-        ax.set_xticklabels(['90°','45°','0°','315°', '270°', '225°', '180°', '135°'])
-
-    def get_discretized_BSDF(self, discr, sigma, kernel="gaussian"):
+    def get_discretized_BSDF(self, discr, sigma):
         """
         Compute the weight associate with each element of the discretization (discr).
         Input :
-            discr (np.array of size (Nx3): Discretization of the sky, N = number of patches
-            sigma (np.array of size (N): area proxy of each patch
+            discr (np.array of size (nPatch, 3):Direction of the sky patches, nPatch = number of patches
+            sigma (np.array of size (nPatch): area proxy of each patch
             kernel (str): projection kernel.
         Output :
-            W_trans (np.array of size (SxN): Weight associate with each patch, S = number of solar position
+            W_trans (np.array of size (nSolPos, nPatch): Weight associate with each patch,
+                                                        nSolPos = number of solar position
         """
         sigma2 = np.concatenate([np.sqrt(sigma),np.sqrt(sigma)])[np.newaxis, np.newaxis, :]
         discr2 = np.repeat(np.array([[1, 1, -1]]), discr.shape[0], axis=0) * discr
         sphere = np.concatenate([discr, discr2], axis=0)
         pts = np.stack([self.x_sr, self.y_sr, self.z_sr], axis=2)
-        g = self.get_integration_kernel(pts, sphere, sigma2, kernel=kernel) #np.exp(-np.arccos(A) ** 2 / (2 * sigma2** 2))
+        g = self.get_integration_kernel(pts, sphere, sigma2) #np.exp(-np.arccos(A) ** 2 / (2 * sigma2** 2))
         norm = g.sum(axis=2)[:, :, np.newaxis]
         g_norm = g/norm
         self.W = np.einsum('ij,ij,ijl->il',self.rho, self.ds, g_norm)    #sum_j self.rho_ij*self.ds_ij *g(A, sigma2)_ijl) --> il #i solar dimension and l sky dimension
         W_trans = self.W[:,:discr.shape[0]]
         return W_trans
 
-    def get_transmission_reflexion_weight(self):
+    def get_transmission_reflection_weight(self):
+        """
+        Give the weight for the reflection and transmission hemisphere.
+        Output:
+            W_trans (np.array of size (nSolPos, nPatch): Weight associate with each patch for the transmission
+            W_refl  (np.array of size (nSolPos, nPatch): Weight associate with each patch for the reflection
+        """
         N = self.W.shape[1]
-        return self.W[:N//2], self.W[N//2:]
+        W_trans = self.W[:N//2]
+        W_refl = self.W[N//2:]
+        return W_trans, W_refl
 
     def transfer_function(self, vect_sun, method, angle_discr):
         return NotImplementedError
 
-    def get_integration_kernel(self, pts, sphere, sigma, kernel='gaussian'):
+    def get_integration_kernel(self, pts, sphere, sigma):
         """
         Compute the integration kernel for each patch
+        Input
+            pts (nPosSol, Nvec, 3): direction of the transmitted rays in the global reference frame
+            sphere (2*nPatch, 3): direction of the sky patches
+            sigma (newaxis, newaxis, 2*nPatch): size of each patch
+
+        Output:
+            g (nSolPos, Nvec, nPatch): radial gaussian integration kernel
         """
         dist = np.einsum('ijk, lk->ijl', pts, sphere)
-        if kernel == 'gaussian':
-            g = np.exp(-np.arccos(dist) ** 2 / (2 * sigma** 2))
-        elif kernel == 'von_mises':
-            g = np.exp(dist*10/sigma)/i0(10/sigma)
-        else:
-            raise("Kernel not implemented. Valid values are 'gaussian' or 'von_mises'")
+        g = np.exp(-np.arccos(dist) ** 2 / (2 * sigma** 2))
         return g
 
 
@@ -137,11 +157,11 @@ class LenticularDiffuser(Diffuser):
         sun vector and the lens vector.
 
         Input :
-            vect_sun : sun vectors
-            res : output angular resolution
+            vect_sun (nSunPos, 3): sun vectors
+            angle_res (scalar): output angular resolution in radians
 
         Return :
-            x, y, z : the direction of the transmitted light in the diffuser frame
+            x, y, z: the direction of the transmitted light in the diffuser frame
         """
         res = self.res*angle_discr if self.res is not None else 0.1
         gamma = self.get_gamma_angle(vect_sun, res)
@@ -160,9 +180,11 @@ class LenticularDiffuser(Diffuser):
         Compute the beta angle (i.e. angle between the diffuser plan and
         the plan created by the sun vector and the lens vector.)
 
-        Input :
-            vect_sun : sun vectors
-            Anle_res : output angular resolution
+        Input:
+            vect_sun (nSunPos, 3): sun vectors
+            angle_res (scalar): output angular resolution in radians
+        Output:
+            beta (nSolPos, Nvec): beta angle in radians
         """
         plan_sunl = np.cross(vect_sun, self.len_vector)
         norm_psl = np.linalg.norm(plan_sunl, axis=1)
@@ -174,17 +196,19 @@ class LenticularDiffuser(Diffuser):
         beta = beta[:, np.newaxis] + beta_t
         return beta
 
-    def get_gamma_angle(self, vect_sun, Angle_res):
+    def get_gamma_angle(self, vect_sun, angle_res):
         """
         Compute the gamma angle (i.e. the Angle between the lens direction
         and the sun direction)
 
         Input :
-            vect_sun : sun vectors
-            Anle_res : output angular resolution
+            vect_sun (nSunPos, 3): sun vectors
+            angle_res (scalar): output angular resolution in radians
+        Output:
+            gamma (nSolPos, Nvec): beta angle in radians, Nvec is the number of segments discretizing the diffuser trace
         """
         cos_gamma = np.clip(np.dot(vect_sun, self.len_vector), -1, 1)
         gamma = np.arccos(cos_gamma)
         gamma = gamma[:, np.newaxis]
-        gamma = np.tile(gamma, (1, int((2 * self.omega + Angle_res) // Angle_res)))
+        gamma = np.tile(gamma, (1, int((2 * self.omega + angle_res) // angle_res)))
         return gamma
