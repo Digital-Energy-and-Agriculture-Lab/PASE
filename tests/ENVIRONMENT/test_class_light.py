70821aeaa394049921e82256d0b2d29e608f1f20
Arnaud Bouvry <abouvry@uliege.be>
2026-03-25 14:12:48 +0100

    [refactor] move module-level weather setup into a session fixture
    All module-level setup code in test_class_light.py (OutputsManager,
    Weather_data, Sun_positions, Light) is moved into a session-scoped
    pytest fixture in a new conftest.py. This prevents PVGIS network calls
    from happening during pytest collection, fixing the CI/CD pipeline
    failures caused by PVGIS API timeouts (issue #243).

--- tests/ENVIRONMENT/test_class_light.py
+++ tests/ENVIRONMENT/test_class_light.py
@@ -9,77 +9,43 @@ Created on Tue Sep 23 11:20:22 2025
 import numpy as np
 import pytest
 
-from pase.DATA_MANAGEMENT.OUTPUT.outputs_manager import OutputsManager
-from pase.DATA_MANAGEMENT.yaml_inputs_provider import YAML_Inputs_provider
-from pase.DATA_MANAGEMENT.weather_data_provider import (Weather_data,
-                                                        get_cache_key,
-                                                        fetch_weather_from_pvgis)
-from pase.ENVIRONMENT.light import Sun_positions, Light
 
-
-Loc_1 = YAML_Inputs_provider(file='Siguesol_loc.yaml', subpath='SCENARIOS').inputs
-
-om = OutputsManager(Loc_1['LocationName'],
-                    Loc_1['SimulationStartingYear'],
-                    Loc_1['SimulationEndingYear'])
-
-variant_dir = om.setup_variant(Loc_1,
-                               av={},
-                               pv_module={},
-                               structure={},
-                               crop_config={'CropModel': ''},
-                               source=__file__)
-
-cache_key = get_cache_key(Loc_1['Latitude'],
-                          Loc_1['Longitude'],
-                          Loc_1['SimulationStartingYear'],
-                          Loc_1['SimulationEndingYear'])
-
-# Import of weather data and computation of daily weather data
-lat = Loc_1['Latitude']
-lon = Loc_1['Longitude']
-start_year = Loc_1['SimulationStartingYear']
-end_year = Loc_1['SimulationEndingYear']
-
-raw_weather = om.load_or_fetch_weather(
-    key=cache_key,
-    fetch_fn=lambda: fetch_weather_from_pvgis(lat, lon, start_year, end_year)
-)
-WD = Weather_data(Loc_1['Latitude'],
-                  Loc_1['Longitude'],
-                  Loc_1['SimulationStartingYear'],
-                  Loc_1['SimulationEndingYear'],
-                  Loc_1['WeatherDataOption'],
-                  raw_weather,
-                  Loc_1['WeatherFileName'],
-                  Loc_1['DailyWeatherFileName']
-                  )
-Sun_positions_complete = Sun_positions(Loc_1['Latitude'],
-                                       Loc_1['Longitude'],
-                                       len(WD.nyears_data[str(Loc_1['SimulationStartingYear'])]),
-                                       Loc_1['TimeZone'])
-
-light_test = Light(WD.nyears_data, Sun_positions_complete)
-
-def test_get_anisotropy_index():
-    
+def test_get_anisotropy_index(light_fixture):
     rad_top_atm = np.array([0, 0, 0, 52.01, 215.54, 453.69, 273.43, 127.08, 0, 0, 0])
     BHI = np.array([0, 0, 0, 1.123, 1.18, 138.43, 250.21, 126.54, 0, 0, 0])
-        
-    assert (light_test.get_anisotropy_index(rad_top_atm, BHI) == np.array([0, 0, 0, 1.123/52.01, 1.18/215.54, 138.43/453.69, 250.21/273.43, 126.54/127.08, 0, 0, 0])).all()
+
+    assert (
+        light_fixture.get_anisotropy_index(rad_top_atm, BHI)
+        == np.array(
+            [
+                0,
+                0,
+                0,
+                1.123 / 52.01,
+                1.18 / 215.54,
+                138.43 / 453.69,
+                250.21 / 273.43,
+                126.54 / 127.08,
+                0,
+                0,
+                0,
+            ]
+        )
+    ).all()
+
 
 @pytest.mark.parametrize(
     "kc_in, cle_in, expected_sky_types",
     [
-        (0.01, 0.0, 1), # Overcast sky
+        (0.01, 0.0, 1),  # Overcast sky
         (0.01, 0.15, 1),
         (0.40, 0.00, 1),
         (0.4001, 0.0001, 1),
-        (0.01, 0.2, 4), # Intermediate overcast sky
+        (0.01, 0.2, 4),  # Intermediate overcast sky
         (0.25, 0.25, 4),
         (0.45, 0.00, 4),
         (0.4501, 0.0001, 4),
-        (0.25, 0.30, 7), # Intermediate sky
+        (0.25, 0.30, 7),  # Intermediate sky
         (0.40, 0.15, 7),
         (0.40, 0.30, 7),
         (0.70, 0.15, 7),
@@ -97,9 +63,9 @@ def test_get_anisotropy_index():
         (1.001, 0.1501, 5),
         (1.25, 1.05, 5),  # undefined sky (high Kc, high Cle)
         (1.2501, 1.0501, 5),
-    ]
+    ],
 )
-def test_get_sky_type(kc_in, cle_in, expected_sky_types):
+def test_get_sky_type(light_fixture, kc_in, cle_in, expected_sky_types):
     """
 
     Parameters
@@ -108,8 +74,6 @@ def test_get_sky_type(kc_in, cle_in, expected_sky_types):
     cle_in list
     expected_sky_type list
     """
-    # Kc_values = np.array(kc_in)
-    # Cle_values = np.array(cle_in)Cle_in
-
-    assert light_test.get_sky_type(np.array([kc_in]), np.array([cle_in])) == [expected_sky_types]
-
+    assert light_fixture.get_sky_type(np.array([kc_in]), np.array([cle_in])) == [
+        expected_sky_types
+    ]
